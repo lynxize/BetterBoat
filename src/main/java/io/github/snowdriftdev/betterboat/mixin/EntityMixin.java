31e9b0fd841a1bc7d97b6ad1ea40ad7c7b794040
lynxize <lynxize@gmail.com>
2023-04-27 21:57:21 -0600

    Un-fix MC-260445

---
+++ src/main/java/io/github/snowdriftdev/betterboat/mixin/EntityMixin.java
@@ -0,0 +1,34 @@
+package io.github.snowdriftdev.betterboat.mixin;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.vehicle.Boat;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(Entity.class)
+public abstract class EntityMixin {
+	@Shadow
+	private float maxUpStep;
+
+	// stupid mandated doc
+	/**
+	 * @author Snowdrift_
+	 * @reason Easiest way to accomplish this,
+	 * and I can't think of any reasons anyone else would want to mixin here
+	 */
+	@Overwrite
+	public float maxUpStep() {
+		if (((Object) this) instanceof Boat)
+			return 1.0F;
+		else return maxUpStep;
+	}
+}
+
+
+
+
+
+
+
+
