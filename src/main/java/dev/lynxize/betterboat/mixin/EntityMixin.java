000fa7fcb455208200fd2bfc458c60faa8e0b328
lynxize <lynxize@gmail.com>
2023-08-28 21:05:58 -0600

    Use @Inject cancellable rather than @Overwrite

--- src/main/java/dev/lynxize/betterboat/mixin/EntityMixin.java
+++ src/main/java/dev/lynxize/betterboat/mixin/EntityMixin.java
@@ -3,26 +3,19 @@ package dev.lynxize.betterboat.mixin;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.vehicle.Boat;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin {
-	@Shadow
-	private float maxUpStep;
 
-	// stupid mandated doc
-
-	/**
-	 * @author Snowdrift_
-	 * @reason Easiest way to accomplish this,
-	 * and I can't think of any reasons anyone else would want to mixin here
-	 */
-	@Overwrite
-	public float maxUpStep() {
+	@Inject(method = "maxUpStep", at = @At("HEAD"), cancellable = true)
+	public void maxUpStep(CallbackInfoReturnable<Float> ci) {
 		//noinspection ConstantValue
-		if (((Object) this) instanceof Boat)
-			return 1.0F;
-		else return maxUpStep;
+		if (((Object) this) instanceof Boat) {
+			ci.setReturnValue(1.0F);
+			ci.cancel();
+		}
 	}
 }
